9b3f5dc5d323d5554c6a1c97b6e5ebbb7f3900bf
tetragrey <graizhon@gmail.com>
2024-04-24 12:01:57 +0200

    Add DataBase.find_entries_where_nested to datastructs. Missing some of the docstring

--- metalbase/datastructs.py
+++ metalbase/datastructs.py
@@ -72,5 +72,73 @@ class DataBase:
         with open(self.filename, mode='w', encoding='utf-8') as f:
             json.dump(self.data, f, indent=4)
 
+    def find_entries_where_nested(self, *args, all_in_one=False, **kwargs):
+        """
+
+        Example:
+        --- Testdb.data
+        {'e1': {'subentry': {'instrument': 'flute', 'food': 'tacos'}, 'name': 'Monica', 'id': 1},
+         'e2': {},
+         'e3': {'sub1': {'instrument': 'flute'}, 'sub2': {'food': 'tacos'}}}
+        --- Testdb.Testdb.find_entries_where_nested(instrument='flute', food='tacos')
+        ['e1', 'e3']
+        --- Testdb.find_entries_where_nested(all_in_one=True, instrument='flute', food='tacos')
+        ['e1']
+
+        :param args: (tuple of strings) the arguments together constitute the path to the dict that holds the
+        nested dicts that are being searched through.
+        :param all_in_one: (
+        :param kwargs: (dict) each kwarg represents a key/value pair that is being checked for. If all these key/value
+        pairs are present inside at least one dictionary nested inside the dictionary specified by the args, the entry
+        they're in will be part of the returned list.
+        :return: (list) the list of entries that contain the key/value pairs specified in the kwargs at the location
+        specified by the args.
+        """
+        entries_list = []
+        for entry_id in self.data:
+            # Create a view for self.data at the specified dictionary
+            dict_view = self.data[entry_id]
+            for path_element in args:
+                dict_view = dict_view[path_element]
+
+            # If all key/value pairs specified need to be in the same dict, then the search is simple
+            if all_in_one:
+                for nested_dict_key in dict_view:
+                    if isinstance(dict_view[nested_dict_key], dict):
+                        if all(target_key in dict_view[nested_dict_key] for target_key in kwargs):
+                            if all(dict_view[nested_dict_key][target_key] == target_value
+                                   for target_key, target_value in kwargs.items()):
+                                entries_list.append(entry_id)
+                                break
+            else:
+                # Go from subdict to subdict searching for the specified key/value pairs. As each is found, it's removed
+                # from pairs_to_find. If pairs_to_find is empty, we know the entry qualifies so it's appended to the list
+                pairs_to_find = kwargs.copy()  # A copy of kwargs where we can cross out any key/value pairs we've found
+                for nested_dict_key in dict_view:
+                    if not pairs_to_find:
+                        break
+                    for target_key, target_value in kwargs.items():
+                        if isinstance(dict_view[nested_dict_key], dict):
+                            if target_key in dict_view[nested_dict_key]:
+                                if dict_view[nested_dict_key][target_key] == target_value:
+                                    del pairs_to_find[target_key]
+                                    if not pairs_to_find:
+                                        entries_list.append(entry_id)
+                                        break
 
 
+        return entries_list
+
+
+if __name__ == "__main__":
+    Testdb = DataBase('test.json')
+    Testdb.create_entry('e1')
+    Testdb.create_entry('e2')
+    Testdb.create_entry('e3')
+    Testdb.add('e1', subentry={}, name='Monica', id=1)
+    Testdb.add('e1', 'subentry', instrument='flute', food='tacos')
+    Testdb.add('e3', sub1={}, sub2={})
+    Testdb.add('e3', 'sub1', instrument='flute')
+    Testdb.add('e3', 'sub2', food='tacos')
+    print(Testdb.find_entries_where_nested(all_in_one=True, instrument='flute', food='tacos'))
+    print(Testdb.data)
